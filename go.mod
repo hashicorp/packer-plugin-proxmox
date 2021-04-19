d1dfb7ea8744797ebb79d2e468bbdef81c648594
Adrien Delorme <azr@users.noreply.github.com>
2021-04-19 15:33:02 +0200

    up main + go module

--- go.mod
+++ go.mod
@@ -1,8 +1,13 @@
 module github.com/hashicorp/packer-plugin-proxmox
 
 go 1.16
+
 require (
+	github.com/Telmate/proxmox-api-go v0.0.0-20210402150908-b9cc6607c070
 	github.com/hashicorp/hcl/v2 v2.9.1
+	github.com/hashicorp/packer v1.7.2
 	github.com/hashicorp/packer-plugin-sdk v0.2.0
+	github.com/mitchellh/mapstructure v1.4.0
+	github.com/stretchr/testify v1.7.0
 	github.com/zclconf/go-cty v1.8.1
 )
--- main.go
+++ main.go
@@ -3,13 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
-	"packer-plugin-scaffolding/builder/scaffolding"
-	scaffoldingData "packer-plugin-scaffolding/datasource/scaffolding"
-	scaffoldingPP "packer-plugin-scaffolding/post-processor/scaffolding"
-	scaffoldingProv "packer-plugin-scaffolding/provisioner/scaffolding"
 
 	"github.com/hashicorp/packer-plugin-sdk/plugin"
 	"github.com/hashicorp/packer-plugin-sdk/version"
+
+	proxmoxclone "github.com/hashicorp/packer-plugin-proxmox/builder/proxmox/clone"
+	proxmoxiso "github.com/hashicorp/packer-plugin-proxmox/builder/proxmox/iso"
 )
 
 var (
@@ -28,10 +27,8 @@ var (
 
 func main() {
 	pps := plugin.NewSet()
-	pps.RegisterBuilder("my-builder", new(scaffolding.Builder))
-	pps.RegisterProvisioner("my-provisioner", new(scaffoldingProv.Provisioner))
-	pps.RegisterPostProcessor("my-post-processor", new(scaffoldingPP.PostProcessor))
-	pps.RegisterDatasource("my-datasource", new(scaffoldingData.Datasource))
+	pps.RegisterBuilder("proxmox-iso", new(proxmoxiso.Builder))
+	pps.RegisterBuilder("proxmox-clone", new(proxmoxclone.Builder))
 	pps.SetVersion(PluginVersion)
 	err := pps.Run()
 	if err != nil {
