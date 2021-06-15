f07145bc78cb0199c15df6a949fd075a7ca1f013
Megan Marsh <megan@hashicorp.com>
2021-06-14 19:24:05 -0700

    updated to 1.0.1-dev

--- main.go
+++ main.go
@@ -13,12 +13,12 @@ import (
 
 var (
 	// Version is the main version number that is being run at the moment.
-	Version = "1.0.0"
+	Version = "1.0.1"
 
 	// VersionPrerelease is A pre-release marker for the Version. If this is ""
 	// (empty string) then it means that it is a final release. Otherwise, this
 	// is a pre-release such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = ""
+	VersionPrerelease = "dev"
 
 	// PluginVersion is used by the plugin set to allow Packer to recognize
 	// what version this plugin is.
