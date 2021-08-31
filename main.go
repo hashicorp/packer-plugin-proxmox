857eb0caf160e9a8a64498e07751c66560185228
Adrien Delorme <azr@users.noreply.github.com>
2021-08-31 15:55:10 +0200

    consolidate version location

--- main.go
+++ main.go
@@ -5,24 +5,10 @@ import (
 	"os"
 
 	"github.com/hashicorp/packer-plugin-sdk/plugin"
-	"github.com/hashicorp/packer-plugin-sdk/version"
 
 	proxmoxclone "github.com/hashicorp/packer-plugin-proxmox/builder/proxmox/clone"
 	proxmoxiso "github.com/hashicorp/packer-plugin-proxmox/builder/proxmox/iso"
-)
-
-var (
-	// Version is the main version number that is being run at the moment.
-	Version = "1.0.1"
-
-	// VersionPrerelease is A pre-release marker for the Version. If this is ""
-	// (empty string) then it means that it is a final release. Otherwise, this
-	// is a pre-release such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = ""
-
-	// PluginVersion is used by the plugin set to allow Packer to recognize
-	// what version this plugin is.
-	PluginVersion = version.InitializePluginVersion(Version, VersionPrerelease)
+	"github.com/hashicorp/packer-plugin-proxmox/version"
 )
 
 func main() {
@@ -30,7 +16,7 @@ func main() {
 	pps.RegisterBuilder(plugin.DEFAULT_NAME, new(proxmoxiso.Builder))
 	pps.RegisterBuilder("proxmox-iso", new(proxmoxiso.Builder))
 	pps.RegisterBuilder("proxmox-clone", new(proxmoxclone.Builder))
-	pps.SetVersion(PluginVersion)
+	pps.SetVersion(version.PluginVersion)
 	err := pps.Run()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
