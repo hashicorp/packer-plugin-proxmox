2f36477a2f1b5bd89d86b2ec787185718e6be52d
Wilken Rivera <dev@wilkenrivera.com>
2022-06-10 09:18:33 -0400

    Cut relese 1.0.8

--- version/version.go
+++ version/version.go
@@ -4,7 +4,7 @@ import "github.com/hashicorp/packer-plugin-sdk/version"
 
 var (
 	// Version is the main version number that is being run at the moment.
-	Version = "1.0.7"
+	Version = "1.0.8"
 
 	// VersionPrerelease is A pre-release marker for the Version. If this is ""
 	// (empty string) then it means that it is a final release. Otherwise, this
