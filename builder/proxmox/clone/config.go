99c7aa1e2e975f4b3c1c8cdc983a5af773602ec1
Sebastian Fleer <dev@dwurp.de>
2022-11-02 16:17:27 +0100

    clone: Add Cloud-Init networking options

--- builder/proxmox/clone/config.go
+++ builder/proxmox/clone/config.go
@@ -1,8 +1,13 @@
-//go:generate packer-sdc mapstructure-to-hcl2 -type Config
+//go:generate packer-sdc mapstructure-to-hcl2 -type Config,cloudInitIpconfig
 
 package proxmoxclone
 
 import (
+	"fmt"
+	"net"
+	"net/netip"
+	"strings"
+
 	proxmox "github.com/hashicorp/packer-plugin-proxmox/builder/proxmox/common"
 	packersdk "github.com/hashicorp/packer-plugin-sdk/packer"
 	"github.com/hashicorp/packer-plugin-sdk/template/config"
@@ -12,7 +17,18 @@ type Config struct {
 	proxmox.Config `mapstructure:",squash"`
 
 	CloneVM   string         `mapstructure:"clone_vm"`
-	FullClone config.Trilean `mapstructure:"full_clone" required:"false"`
+	FullClone config.Trilean `mapstructure:"full_clone"`
+
+	Nameserver   string              `mapstructure:"nameserver" required:"false"`
+	Searchdomain string              `mapstructure:"searchdomain" required:"false"`
+	Ipconfigs    []cloudInitIpconfig `mapstructure:"ipconfig" required:"false"`
+}
+
+type cloudInitIpconfig struct {
+	Ip       string `mapstructure:"ip" required:"false"`
+	Gateway  string `mapstructure:"gateway" required:"false"`
+	Ip6      string `mapstructure:"ip6" required:"false"`
+	Gateway6 string `mapstructure:"gateway6" required:"false"`
 }
 
 func (c *Config) Prepare(raws ...interface{}) ([]string, []string, error) {
@@ -22,8 +38,65 @@ func (c *Config) Prepare(raws ...interface{}) ([]string, []string, error) {
 		errs = packersdk.MultiErrorAppend(errs, merrs)
 	}
 
+	// Check validity of given IP addresses
+	if c.Nameserver != "" {
+		for _, nameserver := range strings.Split(c.Nameserver, " ") {
+			_, err := netip.ParseAddr(nameserver)
+			if err != nil {
+				errs = packersdk.MultiErrorAppend(errs, fmt.Errorf("could not parse nameserver: %s", err))
+			}
+		}
+	}
+	for _, i := range c.Ipconfigs {
+		if i.Ip != "" {
+			_, _, err := net.ParseCIDR(i.Ip)
+			if err != nil {
+				errs = packersdk.MultiErrorAppend(errs, fmt.Errorf("could not parse ipconfig.ip: %s", err))
+			}
+		}
+		if i.Gateway != "" {
+			_, err := netip.ParseAddr(i.Gateway)
+			if err != nil {
+				errs = packersdk.MultiErrorAppend(errs, fmt.Errorf("could not parse ipconfig.gateway: %s", err))
+			}
+		}
+		if i.Ip6 != "" {
+			_, _, err := net.ParseCIDR(i.Ip6)
+			if err != nil {
+				errs = packersdk.MultiErrorAppend(errs, fmt.Errorf("could not parse ipconfig.ip6: %s", err))
+			}
+		}
+		if i.Gateway6 != "" {
+			_, err := netip.ParseAddr(i.Gateway6)
+			if err != nil {
+				errs = packersdk.MultiErrorAppend(errs, fmt.Errorf("could not parse ipconfig.gateway6: %s", err))
+			}
+		}
+	}
+	if len(c.NICs) < len(c.Ipconfigs) {
+		errs = packersdk.MultiErrorAppend(errs, fmt.Errorf("%d ipconfig blocks given, but only %d network interfaces defined", len(c.Ipconfigs), len(c.NICs)))
+	}
+
 	if errs != nil && len(errs.Errors) > 0 {
 		return nil, warnings, errs
 	}
 	return nil, warnings, nil
+}
+
+// Convert Ipconfig attributes into a Proxmox-API compatible string
+func (c cloudInitIpconfig) String() string {
+	options := []string{}
+	if c.Ip != "" {
+		options = append(options, "ip="+c.Ip)
+	}
+	if c.Gateway != "" {
+		options = append(options, "gw="+c.Gateway)
+	}
+	if c.Ip6 != "" {
+		options = append(options, "ip6="+c.Ip6)
+	}
+	if c.Gateway6 != "" {
+		options = append(options, "gw6="+c.Gateway6)
+	}
+	return strings.Join(options, ",")
 }
