8db4c671ee29c17f0fb5984c1813a040fb64c49d
Lucas Bajolet <lucas.bajolet@hashicorp.com>
2023-03-03 09:04:02 -0500

    version: prepare 1.1.3-dev

--- version/version.go
+++ version/version.go
@@ -7,12 +7,12 @@ import "github.com/hashicorp/packer-plugin-sdk/version"
 
 var (
 	// Version is the main version number that is being run at the moment.
-	Version = "1.1.2"
+	Version = "1.1.3"
 
 	// VersionPrerelease is A pre-release marker for the Version. If this is ""
 	// (empty string) then it means that it is a final release. Otherwise, this
 	// is a pre-release such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = ""
+	VersionPrerelease = "dev"
 
 	// PluginVersion is used by the plugin set to allow Packer to recognize
 	// what version this plugin is.
