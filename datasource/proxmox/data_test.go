ca2575f5338eacf5d84585b0cfca2540c7067a78
Castor Sky <csky57@gmail.com>
2024-10-20 22:14:53 +0300

    edit: pack of test for execute() ready

--- datasource/proxmox/data_test.go
+++ datasource/proxmox/data_test.go
@@ -10,10 +10,29 @@ import (
 	"testing"
 )
 
-const mockResourcesResponse = `{"data":[{"diskread":0,"netout":0,"type":"qemu","name":"first-vm","vmid":100,"netin":0,"id":"qemu/100","maxmem":2147483648,"diskwrite":0,"maxcpu":1,"template":0,"status":"stopped","cpu":0,"mem":0,"uptime":0,"node":"pve","disk":0,"maxdisk":34359738368},{"netin":0,"maxmem":2147483648,"id":"qemu/101","diskwrite":0,"status":"stopped","maxcpu":1,"template":0,"diskread":0,"type":"qemu","netout":0,"name":"second-vm","tags":"blue;red","vmid":101,"node":"pve","disk":0,"maxdisk":34359738368,"cpu":0,"uptime":0,"mem":0},{"maxdisk":34359738368,"disk":0,"node":"pve","mem":0,"uptime":0,"cpu":0,"maxcpu":1,"template":1,"status":"stopped","diskwrite":0,"maxmem":2147483648,"id":"qemu/102","netin":0,"vmid":102,"tags":"blue","type":"qemu","netout":0,"name":"template-three","diskread":0}]}`
-const mockVmConfig100Response = `{"data":{"net0":"virtio=BC:24:11:30:AD:93,bridge=vmbr0,firewall=1","ostype":"l26","digest":"1ac0a42d3ead982a5631ee77a9444b44e5ff5456","meta":"creation-qemu=8.1.5,ctime=1729285344","cpu":"x86-64-v2-AES","numa":0,"ide2":"none,media=cdrom","scsihw":"virtio-scsi-single","cores":1,"sockets":1,"boot":"order=scsi0;ide2;net0","vmgenid":"fe9dbfef-eb32-4c75-9e36-aaefc3cbda82","smbios1":"uuid=413c6ca3-d043-43fe-9fb4-bc4a6e60c7ef","memory":"2048","name":"first-vm","scsi0":"local-lvm:vm-100-disk-0,iothread=1,size=32G"}}`
-const mockVmConfig101Response = `{"data":{"digest":"a3a5061cd0f35f0aa14fd98b20095963d53cd225","net0":"virtio=BC:24:11:FA:B6:AB,bridge=vmbr0,firewall=1","ostype":"l26","numa":0,"cpu":"x86-64-v2-AES","meta":"creation-qemu=8.1.5,ctime=1729285359","boot":"order=scsi0;ide2;net0","sockets":1,"cores":1,"scsihw":"virtio-scsi-single","ide2":"none,media=cdrom","scsi0":"local-lvm:vm-101-disk-0,iothread=1,size=32G","name":"second-vm","memory":"2048","vmgenid":"48935acd-21f9-4733-a695-5072949deab5","tags":"blue;red","smbios1":"uuid=271b5345-f952-4c99-a299-b66d71b4e736"}}`
-const mockVmConfig102Response = `{"data":{"memory":"2048","smbios1":"uuid=95ef26a6-78e7-46f9-9728-9a222213f198","ide2":"none,media=cdrom","template":1,"cores":1,"meta":"creation-qemu=8.1.5,ctime=1729285377","cpu":"x86-64-v2-AES","digest":"03ef3384bc6f151328671273f32d0df85ebe257f","net0":"virtio=BC:24:11:A4:A0:3E,bridge=vmbr0,firewall=1","ostype":"l26","tags":"blue","vmgenid":"364e8932-f1ad-4cb2-add8-c720a891c654","name":"template-three","scsi0":"local-lvm:base-102-disk-0,iothread=1,size=32G","sockets":1,"scsihw":"virtio-scsi-single","boot":"order=scsi0;ide2;net0","numa":0}}`
+// For the sake of saving code clean have left only test-related fields in these JSONs.
+const mockResourcesResponse = `{"data":[
+	{"type":"qemu","name":"first-vm","vmid":100,"id":"qemu/100","template":0,"node":"pve"},
+	{"id":"qemu/101","template":0,"type":"qemu","name":"second-vm","tags":"blue;red","vmid":101,"node":"pve"},
+	{"node":"pve","template":1,"id":"qemu/102","vmid":102,"tags":"blue","type":"qemu","name":"template-three"}
+]}`
+const mockVmConfig100Response = `{"data":
+	{"meta":"creation-qemu=8.1.5,ctime=1729285344","name":"first-vm"}
+}`
+const mockVmConfig101Response = `{"data":
+	{"meta":"creation-qemu=8.1.5,ctime=1729285359","name":"second-vm","tags":"blue;red"}
+}`
+const mockVmConfig102Response = `{"data":
+	{"template":1,"meta":"creation-qemu=8.1.5,ctime=1729285377","tags":"blue","name":"template-three"}
+}`
+
+// All configs have to have all default fields so add them from `configDefault` (in place).
+func (c *Config) saturateWithDefault(configDefault Config) {
+	c.proxmoxURL = configDefault.proxmoxURL
+	c.SkipCertValidation = configDefault.SkipCertValidation
+	c.Username = configDefault.Username
+	c.Token = configDefault.Token
+}
 
 func TestExecute(t *testing.T) {
 	mockAPI := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -37,22 +56,119 @@ func TestExecute(t *testing.T) {
 	defer mockAPI.Close()
 
 	pxmxURL, _ := url.Parse(mockAPI.URL)
-	config := Config{
+	defaultConfig := Config{
 		proxmoxURL:         pxmxURL,
 		SkipCertValidation: true,
 		Username:           "dummy@vmhost",
 		Token:              "dummy",
-		Name:               "second-vm",
-		//Latest:             true,
 	}
 
-	ds := Datasource{
-		config: config,
+	dsTestConfigs := []struct {
+		name          string
+		expectFailure bool
+		expectedVmId  int64
+		configDiff    Config
+	}{
+		{
+			name:          "guest with name first-vm found, no error",
+			expectFailure: false,
+			expectedVmId:  100,
+			configDiff: Config{
+				Name: "first-vm",
+			},
+		},
+		{
+			name:          "no existent guest matches filter, error",
+			expectFailure: true,
+			configDiff: Config{
+				Name: "firstest-vm",
+			},
+		},
+		{
+			name:          "found guest by regex filter, no error",
+			expectFailure: false,
+			expectedVmId:  101,
+			configDiff: Config{
+				NameRegex: "sec.*",
+			},
+		},
+		{
+			name:          "multiple guests match the regex, but latest not used, error",
+			expectFailure: true,
+			configDiff: Config{
+				NameRegex: ".*-vm",
+			},
+		},
+		{
+			name:          "multiple guests match the regex and latest used, error",
+			expectFailure: false,
+			expectedVmId:  101,
+			configDiff: Config{
+				NameRegex: ".*-vm",
+				Latest:    true,
+			},
+		},
+		{
+			name:          "found guest that is template, no error",
+			expectFailure: false,
+			expectedVmId:  102,
+			configDiff: Config{
+				Template: true,
+			},
+		},
+		{
+			name:          "found latest guest at node, no error",
+			expectFailure: false,
+			expectedVmId:  102,
+			configDiff: Config{
+				Node:   "pve",
+				Latest: true,
+			},
+		},
+		{
+			name:          "proxmox host not found, error",
+			expectFailure: true,
+			configDiff: Config{
+				Node: "proxmox-host",
+			},
+		},
+		{
+			name:          "found guest with set of tags, no error",
+			expectFailure: false,
+			expectedVmId:  101,
+			configDiff: Config{
+				VmTags: "blue;red",
+			},
+		},
+		{
+			name:          "found multiple guests with tag, error",
+			expectFailure: true,
+			configDiff: Config{
+				VmTags: "blue",
+			},
+		},
 	}
 
-	result, err := ds.Execute()
-	require.NoError(t, err)
-	t.Log(result)
+	for _, dsTestConfig := range dsTestConfigs {
+		t.Run(dsTestConfig.name, func(t *testing.T) {
+			dsTestConfig.configDiff.saturateWithDefault(defaultConfig)
+			ds := Datasource{
+				config: dsTestConfig.configDiff,
+			}
+
+			result, err := ds.Execute()
+			if err != nil && !dsTestConfig.expectFailure {
+				t.Fatalf("unexpected failure: %s", err)
+			}
+			if err == nil && dsTestConfig.expectFailure {
+				t.Errorf("expected failure, but execution succeeded")
+			}
+			if err == nil {
+				vmIdInt64, _ := result.GetAttr("vm_id").AsBigFloat().Int64()
+				require.Equal(t, dsTestConfig.expectedVmId, vmIdInt64)
+			}
+		})
+	}
 }
 
 func TestParseMetaField(t *testing.T) {
