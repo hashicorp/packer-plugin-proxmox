2c1c9e26643184917355caf4bc8f5ac123302aef
Martin Pywell <mpywell@gmail.com>
2024-11-06 21:53:58 +1100

    common: keep generated ISOs when unmount is false

--- builder/proxmox/common/step_upload_iso.go
+++ builder/proxmox/common/step_upload_iso.go
@@ -91,6 +91,11 @@ func (s *stepUploadISO) Cleanup(state multistep.StateBag) {
 	ui := state.Get("ui").(packersdk.Ui)
 	client := state.Get("proxmoxClient").(uploader)
 
+	// If everything finished successfully and we want to keep the ISO mounted, don't cleanup generated ISO
+	if _, ok := state.GetOk("success"); ok && !s.ISO.Unmount {
+		return
+	}
+
 	if (len(s.ISO.CDFiles) > 0 || len(s.ISO.CDContent) > 0) && s.ISO.DownloadPathKey != "" {
 		// Fake a VM reference, DeleteVolume just needs the node to be valid
 		vmRef := &proxmoxapi.VmRef{}
