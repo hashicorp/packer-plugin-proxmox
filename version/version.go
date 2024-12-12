0ac9785f582049d0bbdc0df5baaf4471e2ad5009
Martin Grogan <martin.grogan@hashicorp.com>
2024-12-12 12:46:48 -0500

    version: prepare v1.2.3-dev

--- version/version.go
+++ version/version.go
@@ -6,8 +6,8 @@ package version
 import "github.com/hashicorp/packer-plugin-sdk/version"
 
 var (
-	Version           = "1.2.2"
-	VersionPrerelease = ""
+	Version           = "1.2.3"
+	VersionPrerelease = "dev"
 	VersionMetadata   = ""
 	PluginVersion     = version.NewPluginVersion(Version, VersionPrerelease, VersionMetadata)
 )
