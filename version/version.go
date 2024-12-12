31e2151367954ab319e639122f79a1c47ad29a73
Martin Grogan <martin.grogan@hashicorp.com>
2024-12-12 12:46:52 -0500

    version: cut release v1.2.2

--- version/version.go
+++ version/version.go
@@ -7,7 +7,7 @@ import "github.com/hashicorp/packer-plugin-sdk/version"
 
 var (
 	Version           = "1.2.2"
-	VersionPrerelease = "dev"
+	VersionPrerelease = ""
 	VersionMetadata   = ""
 	PluginVersion     = version.NewPluginVersion(Version, VersionPrerelease, VersionMetadata)
 )
